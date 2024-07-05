a9597d4c6e767a59af4d4f6ceb84532daacf56c7
longhainguyen <21020349@vnu.edu.vn>
2024-07-05 10:52:42 +0700

    manage client

--- src/client/dto/create-client.dto.ts
+++ src/client/dto/create-client.dto.ts
@@ -1,1 +1,9 @@
-export class CreateClientDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateClientDto {
+    @IsNotEmpty({ message: 'The clientName is required' })
+    clientName: string;
+
+    @IsNotEmpty({ message: 'The contactInfo is required' })
+    contactInfo: string;
+}
