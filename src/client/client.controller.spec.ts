f5bc88dcb944c1832aff0bf5960f9022894eb4c3
longhainguyen <21020349@vnu.edu.vn>
2024-07-11 11:51:09 +0700

    testing client

--- src/client/client.controller.spec.ts
+++ src/client/client.controller.spec.ts
@@ -1,20 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientController } from './client.controller';
 import { ClientService } from './client.service';
+import { Client } from './entities/client.entity';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateClientDto } from './dto/create-client.dto';
+import { UpdateClientDto } from './dto/update-client.dto';
 
 describe('ClientController', () => {
     let controller: ClientController;
+    let clientService: ClientService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [ClientController],
-            providers: [ClientService],
+            providers: [
+                ClientService,
+                {
+                    provide: getRepositoryToken(Client),
+                    useValue: {},
+                },
+                {
+                    provide: getRepositoryToken(User),
+                    useValue: {},
+                },
+            ],
         }).compile();
 
         controller = module.get<ClientController>(ClientController);
+        clientService = module.get<ClientService>(ClientService);
     });
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
+    });
+
+    describe('create client', () => {
+        it('should create a new client', async () => {
+            const createClientDto: CreateClientDto = {
+                clientName: 'name',
+                contactInfo: 'contact',
+            };
+
+            const result = { id: 1, ...createClientDto } as Client;
+            jest.spyOn(clientService, 'create').mockResolvedValue(result);
+
+            expect(await controller.create({ user: {} }, createClientDto)).toBe(result);
+            expect(clientService.create).toHaveBeenCalledWith({ user: {} }, createClientDto);
+        });
+    });
+
+    describe('update', () => {
+        it('should update a client', async () => {
+            const id = 1;
+            const updateClientDto: UpdateClientDto = { clientName: 'Updated Client' };
+            const result: Client = { id, ...updateClientDto } as Client;
+            jest.spyOn(clientService, 'update').mockResolvedValue(result);
+
+            expect(await controller.update(id + '', updateClientDto)).toBe(result);
+            expect(clientService.update).toHaveBeenCalledWith(id, updateClientDto);
+        });
+    });
+
+    describe('findOne', () => {
+        it('should return a client by id', async () => {
+            const id = 1;
+            const result: Client = {
+                id,
+                clientName: 'Test Client',
+                contactInfo: 'contact',
+            } as Client;
+            jest.spyOn(clientService, 'findOne').mockResolvedValue(result);
+
+            expect(await controller.findOne(id + '')).toBe(result);
+            expect(clientService.findOne).toHaveBeenCalledWith(id);
+        });
     });
 });
--- src/client/client.service.spec.ts
+++ src/client/client.service.spec.ts
@@ -1,17 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientService } from './client.service';
 import { ClientController } from './client.controller';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Client } from './entities/client.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 describe('ClientService', () => {
     let clientService: ClientService;
-    let clientControler: ClientController;
+    let clientRepository: Repository<Client>;
+    let userRepository: Repository<User>;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [ClientService],
+            providers: [
+                ClientService,
+                {
+                    provide: getRepositoryToken(Client),
+                    useValue: {},
+                },
+                {
+                    provide: getRepositoryToken(User),
+                    useValue: {},
+                },
+            ],
         }).compile();
 
         clientService = module.get<ClientService>(ClientService);
+        clientRepository = module.get(getRepositoryToken(Client));
+        userRepository = module.get(getRepositoryToken(User));
     });
 
     it('should be defined', () => {
