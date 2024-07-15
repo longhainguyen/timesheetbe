cb09df3ff3dfdd98b64d63569c4c14eafc715d9a
longhainguyen <21020349@vnu.edu.vn>
2024-07-15 15:09:36 +0700

    prettier file migrations

--- db/migrations/1719885226365-NewMigrations.ts
+++ db/migrations/1719885226365-NewMigrations.ts
@@ -4,6 +4,31 @@ export class NewMigrations1719885226365 implements MigrationInterface {
     name = 'NewMigrations1719885226365';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE \`user\` (
+                \`id\` int NOT NULL AUTO_INCREMENT,
+                \`userName\` varchar(255) NOT NULL UNIQUE,
+                \`email\` varchar(255) NOT NULL UNIQUE,
+                \`password\` varchar(255) NOT NULL,
+                \`role\` enum('staff', 'manager', 'admin') NULL,
+                \`branch\` enum('Ha noi 1', 'Ha noi 2', 'Da nang') NULL,
+                \`isActive\` boolean NOT NULL DEFAULT true,
+                \`createAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                \`updateAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                PRIMARY KEY (\`id\`)
+            ) ENGINE=InnoDB;
+        `);
+        await queryRunner.query(`
+            CREATE TABLE \`project\` (
+                \`id\` int NOT NULL AUTO_INCREMENT,
+                \`projectName\` varchar(255) NOT NULL,
+                \`description\` varchar(255) NOT NULL,
+                \`inActive\` boolean NOT NULL DEFAULT true,
+                \`createAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                \`updateAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                PRIMARY KEY (\`id\`)
+            ) ENGINE=InnoDB;
+        `);
         await queryRunner.query(
             `CREATE TABLE \`user_project\` (\`id\` int NOT NULL AUTO_INCREMENT, \`createAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updateAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`userId\` int NULL, \`projectId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
         );
@@ -22,6 +47,8 @@ export class NewMigrations1719885226365 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE \`user\``);
+        await queryRunner.query(`DROP TABLE \`project\``);
         await queryRunner.query(`ALTER TABLE \`user_project\` DROP FOREIGN KEY \`FK_cb5415b5e54f476329451212e9b\``);
         await queryRunner.query(`ALTER TABLE \`user_project\` DROP FOREIGN KEY \`FK_b88a18e4faeea3bce60d70a4ae3\``);
         await queryRunner.query(
--- db/migrations/1720430578388-addImageToUser.ts
+++ db/migrations/1720430578388-addImageToUser.ts
@@ -1,66 +1,29 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddImageToUser1720430578388 implements MigrationInterface {
-    name = 'AddImageToUser1720430578388'
+    name = 'AddImageToUser1720430578388';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`user_image\` (\`id\` int NOT NULL AUTO_INCREMENT, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(
+            `  CREATE TABLE \`user_image\` (
+                \`id\` int NOT NULL AUTO_INCREMENT,
+                \`path\` varchar(255) NOT NULL,
+                PRIMARY KEY (\`id\`)
+            ) ENGINE=InnoDB;`,
+        );
         await queryRunner.query(`ALTER TABLE \`user\` ADD \`userImageId\` int NULL`);
-        await queryRunner.query(`ALTER TABLE \`user\` ADD UNIQUE INDEX \`IDX_2011c646401c43cf2f7c1f9af4\` (\`userImageId\`)`);
-        await queryRunner.query(`ALTER TABLE \`client\` DROP FOREIGN KEY \`FK_ad3b4bf8dd18a1d467c5c0fc13a\``);
-        await queryRunner.query(`ALTER TABLE \`client\` CHANGE \`userId\` \`userId\` int NULL`);
-        await queryRunner.query(`ALTER TABLE \`user_project\` DROP FOREIGN KEY \`FK_b88a18e4faeea3bce60d70a4ae3\``);
-        await queryRunner.query(`ALTER TABLE \`user_project\` DROP FOREIGN KEY \`FK_cb5415b5e54f476329451212e9b\``);
-        await queryRunner.query(`DROP INDEX \`IDX_2a37107e0b3bdb06b4920a64bb\` ON \`user_project\``);
-        await queryRunner.query(`ALTER TABLE \`user_project\` CHANGE \`userId\` \`userId\` int NULL`);
-        await queryRunner.query(`ALTER TABLE \`user_project\` CHANGE \`projectId\` \`projectId\` int NULL`);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` DROP FOREIGN KEY \`FK_ac58894ff3ba9e26707b7528ecd\``);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` DROP FOREIGN KEY \`FK_689d5a0fd3a37c30aab320afd8e\``);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` DROP FOREIGN KEY \`FK_a147ecd5c870c35058f8437b425\``);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` CHANGE \`projectId\` \`projectId\` int NULL`);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` CHANGE \`userId\` \`userId\` int NULL`);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` CHANGE \`taskId\` \`taskId\` int NULL`);
-        await queryRunner.query(`ALTER TABLE \`user\` CHANGE \`role\` \`role\` enum ('staff', 'manager', 'admin') NULL`);
-        await queryRunner.query(`ALTER TABLE \`user\` CHANGE \`branch\` \`branch\` enum ('Ha noi 1', 'Ha noi 2', 'Da nang') NULL`);
-        await queryRunner.query(`CREATE UNIQUE INDEX \`IDX_2a37107e0b3bdb06b4920a64bb\` ON \`user_project\` (\`userId\`, \`projectId\`)`);
-        await queryRunner.query(`CREATE UNIQUE INDEX \`REL_2011c646401c43cf2f7c1f9af4\` ON \`user\` (\`userImageId\`)`);
-        await queryRunner.query(`ALTER TABLE \`client\` ADD CONSTRAINT \`FK_ad3b4bf8dd18a1d467c5c0fc13a\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`user_project\` ADD CONSTRAINT \`FK_b88a18e4faeea3bce60d70a4ae3\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`user_project\` ADD CONSTRAINT \`FK_cb5415b5e54f476329451212e9b\` FOREIGN KEY (\`projectId\`) REFERENCES \`project\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` ADD CONSTRAINT \`FK_ac58894ff3ba9e26707b7528ecd\` FOREIGN KEY (\`projectId\`) REFERENCES \`project\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` ADD CONSTRAINT \`FK_689d5a0fd3a37c30aab320afd8e\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` ADD CONSTRAINT \`FK_a147ecd5c870c35058f8437b425\` FOREIGN KEY (\`taskId\`) REFERENCES \`task\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`user\` ADD CONSTRAINT \`FK_2011c646401c43cf2f7c1f9af4c\` FOREIGN KEY (\`userImageId\`) REFERENCES \`user_image\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(
+            `ALTER TABLE \`user\` ADD UNIQUE INDEX \`IDX_2011c646401c43cf2f7c1f9af4\` (\`userImageId\`)`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE \`user\` ADD CONSTRAINT \`FK_2011c646401c43cf2f7c1f9af4c\` FOREIGN KEY (\`userImageId\`) REFERENCES \`user_image\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`user\` DROP FOREIGN KEY \`FK_2011c646401c43cf2f7c1f9af4c\``);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` DROP FOREIGN KEY \`FK_a147ecd5c870c35058f8437b425\``);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` DROP FOREIGN KEY \`FK_689d5a0fd3a37c30aab320afd8e\``);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` DROP FOREIGN KEY \`FK_ac58894ff3ba9e26707b7528ecd\``);
-        await queryRunner.query(`ALTER TABLE \`user_project\` DROP FOREIGN KEY \`FK_cb5415b5e54f476329451212e9b\``);
-        await queryRunner.query(`ALTER TABLE \`user_project\` DROP FOREIGN KEY \`FK_b88a18e4faeea3bce60d70a4ae3\``);
-        await queryRunner.query(`ALTER TABLE \`client\` DROP FOREIGN KEY \`FK_ad3b4bf8dd18a1d467c5c0fc13a\``);
-        await queryRunner.query(`DROP INDEX \`REL_2011c646401c43cf2f7c1f9af4\` ON \`user\``);
-        await queryRunner.query(`DROP INDEX \`IDX_2a37107e0b3bdb06b4920a64bb\` ON \`user_project\``);
-        await queryRunner.query(`ALTER TABLE \`user\` CHANGE \`branch\` \`branch\` enum ('Ha noi 1', 'Ha noi 2', 'Da nang') NULL DEFAULT 'NULL'`);
-        await queryRunner.query(`ALTER TABLE \`user\` CHANGE \`role\` \`role\` enum ('staff', 'manager', 'admin') NULL DEFAULT 'NULL'`);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` CHANGE \`taskId\` \`taskId\` int NULL DEFAULT 'NULL'`);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` CHANGE \`userId\` \`userId\` int NULL DEFAULT 'NULL'`);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` CHANGE \`projectId\` \`projectId\` int NULL DEFAULT 'NULL'`);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` ADD CONSTRAINT \`FK_a147ecd5c870c35058f8437b425\` FOREIGN KEY (\`taskId\`) REFERENCES \`task\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` ADD CONSTRAINT \`FK_689d5a0fd3a37c30aab320afd8e\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`timesheet\` ADD CONSTRAINT \`FK_ac58894ff3ba9e26707b7528ecd\` FOREIGN KEY (\`projectId\`) REFERENCES \`project\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`user_project\` CHANGE \`projectId\` \`projectId\` int NULL DEFAULT 'NULL'`);
-        await queryRunner.query(`ALTER TABLE \`user_project\` CHANGE \`userId\` \`userId\` int NULL DEFAULT 'NULL'`);
-        await queryRunner.query(`CREATE UNIQUE INDEX \`IDX_2a37107e0b3bdb06b4920a64bb\` ON \`user_project\` (\`userId\`, \`projectId\`)`);
-        await queryRunner.query(`ALTER TABLE \`user_project\` ADD CONSTRAINT \`FK_cb5415b5e54f476329451212e9b\` FOREIGN KEY (\`projectId\`) REFERENCES \`project\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`user_project\` ADD CONSTRAINT \`FK_b88a18e4faeea3bce60d70a4ae3\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`client\` CHANGE \`userId\` \`userId\` int NULL DEFAULT 'NULL'`);
-        await queryRunner.query(`ALTER TABLE \`client\` ADD CONSTRAINT \`FK_ad3b4bf8dd18a1d467c5c0fc13a\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`user\` DROP INDEX \`IDX_2011c646401c43cf2f7c1f9af4\``);
         await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`userImageId\``);
         await queryRunner.query(`DROP TABLE \`user_image\``);
     }
-
 }
