4bb75adf2b2b5110012ff238d816e8dae22cbb95
longhainguyen <21020349@vnu.edu.vn>
2024-07-22 14:08:19 +0700

    dockerize

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,8 +1,27 @@
+import * as fs from 'fs';
 import { ConfigService } from '@nestjs/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { config } from 'dotenv';
 
+// Load environment variables from .env file
 config();
+
+// Read secrets from files and set environment variables
+const secrets = {
+    SECRET_KEY: '/run/secrets/secret_key',
+    DATABASE_PASSWORD: '/run/secrets/db_password',
+    CLOUDINARY_API_SECRET: '/run/secrets/cloud_api_secret',
+    MAILDEV_INCOMING_PASS: '/run/secrets/mail_pass',
+};
+
+for (const [key, path] of Object.entries(secrets)) {
+    try {
+        const value = fs.readFileSync(path, 'utf8').trim();
+        process.env[key] = value;
+    } catch (error) {
+        console.error(`Error reading secret ${key} from ${path}`, error);
+    }
+}
 
 const configService = new ConfigService();
 
